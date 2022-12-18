6c02aefb4d41e689652c1c4f8a5d49c387c75f32
Isaac <fimbresisaac23@gmail.com>
2022-12-17 17:00:57 -0800

    implementing basic routing in the http server

--- modules/http.js
+++ modules/http.js
@@ -1,7 +1,15 @@
 const http = require('http');
 
 const server = http.createServer((request, result) => {
-    result.write('Welcome to the server!');
+    if(request.url === '/'){
+        result.write('<h1>Welcome to home page.</h1>'); 
+    }
+    else if(request.url === '/about') {
+        result.write('<h1>Welcome to about page.</h1>'); 
+    }
+    else{
+        result.write('<h1>Error: not found!</h1>');
+    }
     result.end();
 })
 

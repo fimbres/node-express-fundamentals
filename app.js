d0a73d6d994c00d926f87fbe02dd19df0aa9deda
Isaac <fimbresisaac23@gmail.com>
2022-12-17 19:34:44 -0800

    http module with event emitters

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
-const EventEmitter = require('events');
+const http = require('http');
 
-const customEmmiter = new EventEmitter();
+const server = http.createServer();
 
-customEmmiter.on('response', () => {
-    console.log("Data response")
+server.on('request', (request, result) => {
+    if(request.url === '/'){
+        result.write('<h1>Welcome to home page.</h1>'); 
+    }
+    else if(request.url === '/about') {
+        result.write('<h1>Welcome to about page.</h1>'); 
+    }
+    else{
+        result.write('<h1>Error: not found!</h1>');
+    }
+    result.end();
 });
 
-customEmmiter.on('response', () => {
-    console.log("Other data in response")
+server.listen(3000, () => {
+    console.log("Server listening in port 3000");
 });
-
-customEmmiter.emit('response');

346768bfcc1360233963e5d999637fd9ab31a8dc
Isaac <fimbresisaac23@gmail.com>
2022-12-20 18:27:57 -0800

    basic implementation of an express ap

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const app = express();
 
+app.get('/', (request, response) => {
+    response.status(200).send('<h1>Home Page</h1>');
+});
+
+app.get('/about', (request, response) => {
+    response.status(200).send('<h1>About Page</h1>');
+});
+
+app.all('*', (request, response) => {
+    response.status(404).send('<h1>Resource Not Found</h1>');
+});
+
 app.listen(5000, () => {
     console.log("The server is listening in port 5000");
 });

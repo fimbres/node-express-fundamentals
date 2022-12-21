14e7b8d73aa22b30f0c0c3e1653ef94e7e9ea6e4
Isaac <fimbresisaac23@gmail.com>
2022-12-20 18:04:20 -0800

    serving static html files from the server side of the API

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const http = require('http');
+const { readFileSync } = require('fs');
+
+const homePage = readFileSync('./pages/index.html', 'utf-8');
 
 http.createServer((request, response) => {
     response.writeHead(200, { 'content-type': 'text/html'});
-    response.write('<h1>Hello World</h1>');
+    response.write(homePage);
     response.end();
 }).listen(5000);

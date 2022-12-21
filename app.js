622c510695a9c3d95f060dfd1d22fcf82e82b941
Isaac <fimbresisaac23@gmail.com>
2022-12-20 19:43:50 -0800

    working with extra params and querys in routes with expressjs

--- app.js
+++ app.js
@@ -6,6 +6,23 @@ app.get('/', (request, response) => {
     response.json({ firstName: "Isaac", lastName: "Fimbres" })
 });
 
+app.get('/products/:productId', (request, response) => {
+    const { productId } = request.params;
+    const isInvalid = productId.match(/[a-z]/i);
+
+    if(!isInvalid){
+        response.status(200).json({ productId });
+    }
+    else{
+        response.status(404).send('Product does not exit');
+    }
+});
+
+app.get('/query', (request, response) => {
+    console.log(request.query)
+    response.json(request.query);
+});
+
 app.listen(5000, () => {
     console.log("Server is listening in port 5000")
 });

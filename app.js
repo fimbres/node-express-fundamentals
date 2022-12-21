2de6e797178caf14aa2433ed28779aa986c52204
Isaac <fimbresisaac23@gmail.com>
2022-12-20 18:19:40 -0800

    new app with expressjs

--- app.js
+++ app.js
@@ -1,22 +1,6 @@
-const http = require('http');
-const { readFileSync } = require('fs');
+const express = require('express');
+const app = express();
 
-const homePage = readFileSync('./pages/home.html', 'utf-8');
-const aboutPage = readFileSync('./pages/about.html', 'utf-8');
-const errorPage = readFileSync('./pages/404.html', 'utf-8');
-
-http.createServer((request, response) => {
-    if(request.url === '/'){
-        response.writeHead(200, { 'content-type': 'text/html'});
-        response.write(homePage);
-        response.end();
-    } else if(request.url === '/about') {
-        response.writeHead(200, { 'content-type': 'text/html'});
-        response.write(aboutPage);
-        response.end();
-    } else {
-        response.writeHead(200, { 'content-type': 'text/html'});
-        response.write(errorPage);
-        response.end();
-    }
-}).listen(5000);
+app.listen(5000, () => {
+    console.log("The server is listening in port 5000");
+});
---
+++ modules/http-html.js
@@ -0,0 +1,22 @@
+const http = require('http');
+const { readFileSync } = require('fs');
+
+const homePage = readFileSync('../pages/home.html', 'utf-8');
+const aboutPage = readFileSync('../pages/about.html', 'utf-8');
+const errorPage = readFileSync('../pages/404.html', 'utf-8');
+
+http.createServer((request, response) => {
+    if(request.url === '/'){
+        response.writeHead(200, { 'content-type': 'text/html'});
+        response.write(homePage);
+        response.end();
+    } else if(request.url === '/about') {
+        response.writeHead(200, { 'content-type': 'text/html'});
+        response.write(aboutPage);
+        response.end();
+    } else {
+        response.writeHead(200, { 'content-type': 'text/html'});
+        response.write(errorPage);
+        response.end();
+    }
+}).listen(5000);
